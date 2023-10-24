44cf6919ac5880a2568776fe3b82fd303b415ebb
Lim200 <112889518+Lim200@users.noreply.github.com>
2023-10-24 12:58:28 +0300

    feat: done task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() { // начальные значения минимума и максимума
+        this.min = null;
+        this.max = null;
+    }
 
-    setRange(min, max) {
-
+    setRange(min, max) { // Заданный диапазон чисел
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        // return Math.floor((this.min + this.max) / 2);
+        return Math.ceil((this.min + this.max) / 2);  // среднее значение текущего диапазона
     }
 
     lower() {
-
+        this.max = this.guess();  // уменьшаем максимальное значение
     }
 
     greater() {
-
+        this.min = this.guess();  //увеличиваем минимальное значение
     }
 }
+
 
 module.exports = GuessingGame;
